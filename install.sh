4874294706a99fc922867d6222a8d6363ed8bae0
alex <skyycr@gmail.com>
2022-06-04 12:01:29 +0200

    documentation

--- install.sh
+++ install.sh
@@ -12,3 +12,4 @@ python3 -m keyring set is-mon password
 
 echo "Install completed. On the first launch of the script, you will be prompted for a webhook"
 echo "If you wish to check your UCO or password, use \"python3 -m keyring get is-mon uco/password\""
+echo "If you wish to change your UCO or password, use \"python3 -m keyring set is-mon uco/password\""
